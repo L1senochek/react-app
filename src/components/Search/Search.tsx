d714e1841411c66d7682d8c2b5853da71318def1
l1senochek <lisenochek96@gmail.com>
2023-10-26 15:58:23 +0300

    feat: add IconLoupe

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import IconLoupe from '../IconLoupe/IconLoupe';
+import './search.scss';
 
 const Search = (): JSX.Element => {
   return (
     <div className="search">
+      <button className="search__loupe">
+        <IconLoupe />
+      </button>
       <input
         className="search__input"
         type="search"
