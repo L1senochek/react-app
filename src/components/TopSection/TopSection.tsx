6a65300a430b716cd31d46c307210baaf9c3aec8
l1senochek <lisenochek96@gmail.com>
2023-10-27 15:22:01 +0300

    refactor: TopSection convert into a class

--- src/components/TopSection/TopSection.tsx
+++ src/components/TopSection/TopSection.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './top-section.scss';
 import Search from '../Search/Search';
 
-const TopSection = (): JSX.Element => {
-  return (
-    <div className="top-section">
-      <div className="top-section__search_wrapper">
-        TopSection
-        <Search />
+class TopSection extends Component {
+  render(): JSX.Element {
+    return (
+      <div className="top-section">
+        <div className="top-section__search_wrapper">
+          TopSection
+          <Search />
+        </div>
+        <hr className="top-section__separator" />
       </div>
-      <hr className="top-section__separator" />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default TopSection;
