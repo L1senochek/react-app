a19449f0ec0f31a71fd8cff5125f8a108da706bc
L1senochek <lisenochek96@gmail.com>
2023-10-28 21:07:30 +0300

    feat: update Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -11,12 +11,12 @@ class Loading extends Component {
           xmlns="http://www.w3.org/2000/svg"
           x="0px"
           y="0px"
-          height={100}
+          height={120}
           viewBox="0 0 100 100"
           enableBackground="new 0 0 0 0"
           xmlSpace="preserve"
         >
-          <circle fill="#15ff00" stroke="none" cx="6" cy="50" r="6">
+          <circle fill="#15ff00" stroke="none" cx="20" cy="50" r="6">
             <animate
               attributeName="opacity"
               dur="1s"
@@ -25,7 +25,7 @@ class Loading extends Component {
               begin="0.1"
             />
           </circle>
-          <circle fill="#15ff00" stroke="none" cx="26" cy="50" r="6">
+          <circle fill="#15ff00" stroke="none" cx="50" cy="50" r="6">
             <animate
               attributeName="opacity"
               dur="1s"
@@ -34,7 +34,7 @@ class Loading extends Component {
               begin="0.2"
             />
           </circle>
-          <circle fill="#15ff00" stroke="none" cx="46" cy="50" r="6">
+          <circle fill="#15ff00" stroke="none" cx="80" cy="50" r="6">
             <animate
               attributeName="opacity"
               dur="1s"
