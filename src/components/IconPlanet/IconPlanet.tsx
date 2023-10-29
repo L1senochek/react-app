de0181f066b9c5752a753d68fc235cd818ad4ca6
L1senochek <lisenochek96@gmail.com>
2023-10-29 19:20:17 +0300

    feat: add media query

--- src/components/IconPlanet/IconPlanet.tsx
+++ src/components/IconPlanet/IconPlanet.tsx
@@ -20,6 +20,7 @@ class IconPlanet extends PureComponent {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
           xmlSpace="preserve"
+          className="icon-planet"
         >
           <path
             fill="#000"
