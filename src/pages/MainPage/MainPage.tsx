5948c12bc704bc12b7d4e7f801a58bfa14816f90
l1senochek <lisenochek96@gmail.com>
2023-10-30 12:06:21 +0300

    refactor: change class MainPage to func component

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ContextType } from 'react';
+import React, { useContext, useEffect } from 'react';
 import TopSection from '../../components/TopSection/TopSection';
 import './main-page.scss';
 import MainSection from '../../components/MainSection/MainSection';
@@ -7,30 +7,31 @@ import Loading from '../../components/Loading/Loading';
 import getPlanets from '../../api/getPlanets';
 import getSearch from '../../api/getSearch';
 
-class MainPage extends Component<Record<string, never>> {
-  static contextType = MainPageContext;
-  declare context: ContextType<typeof MainPageContext>;
+const MainPage = (): JSX.Element => {
+  const { searchValue, setArrRes, setIsLoading, isLoading } =
+    useContext(MainPageContext);
 
-  componentDidMount = async (): Promise<void> => {
-    const { searchValue, setArrRes, setIsLoading } = this.context;
-    if (searchValue === '') {
-      const allPlanets = await getPlanets();
-      setArrRes(allPlanets.results);
-    } else {
-      const getSearchRes = await getSearch(searchValue);
-      setArrRes(getSearchRes.results);
-    }
-    setIsLoading(false);
-  };
+  useEffect((): void => {
+    (async (): Promise<void> => {
+      if (searchValue === '') {
+        const allPlanets = await getPlanets();
+        setArrRes(allPlanets.results);
+        console.log(1);
+      } else {
+        const getSearchRes = await getSearch(searchValue);
+        setArrRes(getSearchRes.results);
+        console.log(2);
+      }
+      setIsLoading(false);
+    })();
+  }, [searchValue, setArrRes, setIsLoading]);
 
-  render = (): JSX.Element => {
-    return (
-      <main className="main-page">
-        <TopSection />
-        {this.context.isLoading ? <Loading /> : <MainSection />}
-      </main>
-    );
-  };
-}
+  return (
+    <main className="main-page">
+      <TopSection />
+      {isLoading ? <Loading /> : <MainSection />}
+    </main>
+  );
+};
 
 export default MainPage;
