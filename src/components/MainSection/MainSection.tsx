69382ecdf5ec58fae49932e244df6b5962f0671c
l1senochek <lisenochek96@gmail.com>
2023-10-30 17:27:48 +0300

    refactor: change class MainSection to FC

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import './main-section.scss';
 import Cards from '../Cards/Cards';
 import ErrorBtn from '../ErrorBtn/ErrorBtn';
 
-class MainSection extends Component {
-  render = (): JSX.Element => {
-    return (
-      <div className="main-section">
-        <h2 className="main-section__title">Planets:</h2>
-        <ErrorBtn />
-        <Cards />
-      </div>
-    );
-  };
-}
+const MainSection: FC = (): JSX.Element => {
+  return (
+    <div className="main-section">
+      <h2 className="main-section__title">Planets:</h2>
+      <ErrorBtn />
+      <Cards />
+    </div>
+  );
+};
 
 export default MainSection;
