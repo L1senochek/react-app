23051d426710cb8c99c4d86637ff5c341a750408
L1senochek <lisenochek96@gmail.com>
2023-11-04 00:18:51 +0300

    feat: update constants

--- src/api/getSearch.ts
+++ src/api/getSearch.ts
@@ -1,8 +1,8 @@
 import IAnime from '../model/api/IAnime';
-import { API_URL_SEARCH } from '../utils/constants/constants';
+import { API_SEARCH_PARAM, API_URL } from '../utils/constants/constants';
 
 const getSearch: (searchValue: string) => Promise<IAnime> = async (
   searchValue
-) => await (await fetch(API_URL_SEARCH + searchValue)).json();
+) => await (await fetch(API_URL + API_SEARCH_PARAM + searchValue)).json();
 
 export default getSearch;
--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'https://api.jikan.moe/v4/anime';
-export const API_URL_SEARCH = 'https://api.jikan.moe/v4/anime?q=';
+export const API_SEARCH_PARAM = '?q=';
