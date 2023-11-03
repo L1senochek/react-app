73d6cc25805c889053628671a5ce35b9e54c083f
l1senochek <lisenochek96@gmail.com>
2023-11-03 12:59:04 +0300

    feat: add NotFound

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+const NotFound: FC = (): JSX.Element => {
+  return (
+    <main className="not-found__wrapper">
+      <h2 className="not-found__message">404</h2>
+    </main>
+  );
+};
+export default NotFound;
--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -4,11 +4,15 @@ import {
   createRoutesFromElements,
 } from 'react-router-dom';
 import MainPage from '../../pages/MainPage/MainPage';
+import NotFound from '../NotFound/NotFound';
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<MainPage />}></Route>
+      <Route path="/">
+        <Route index element={<MainPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </>
   )
 );
