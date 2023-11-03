3e1f9fd99ba5580fdc416f225d7876e87e634e60
L1senochek <lisenochek96@gmail.com>
2023-11-03 21:34:42 +0300

    feat: add Pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Pagination: FC = (): JSX.Element => {
+  return (
+    <ul className="pagination">
+      <li></li>
+    </ul>
+  );
+};
+
+export default Pagination;
