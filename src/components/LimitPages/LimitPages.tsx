0f3afdd169c21033fe5a825dd893acb2b28a9a6f
L1senochek <lisenochek96@gmail.com>
2023-11-05 10:33:45 +0300

    refactor: update LimitPages & add active class

--- src/components/LimitPages/LimitPages.tsx
+++ src/components/LimitPages/LimitPages.tsx
@@ -8,29 +8,23 @@ import {
 import './limit-pages.scss';
 
 const LimitPages: FC = () => {
-  const { pageNum } = useParams();
+  const { pageNum, limitNum } = useParams();
+  const limitArr = [MIN_LIMIT_PAGES, AVG_LIMIT_PAGES, MAX_LIMIT_PAGES];
 
   return (
     <div className="limit-pages">
       <p className="limit-pages__title">Limit pages: </p>
-      <Link
-        className="limit-pages__btn btn"
-        to={`/page/${pageNum}/limit/${MIN_LIMIT_PAGES}`}
-      >
-        {MIN_LIMIT_PAGES}
-      </Link>
-      <Link
-        className="limit-pages__btn btn"
-        to={`/page/${pageNum}/limit/${AVG_LIMIT_PAGES}`}
-      >
-        {AVG_LIMIT_PAGES}
-      </Link>
-      <Link
-        className="limit-pages__btn btn"
-        to={`/page/${pageNum}/limit/${MAX_LIMIT_PAGES}`}
-      >
-        {MAX_LIMIT_PAGES}
-      </Link>
+      {limitArr.map((limit) => (
+        <Link
+          key={`limit-pages${limit}`}
+          className={`limit-pages__btn btn ${
+            limitNum && +limitNum === limit ? 'active' : ''
+          }`}
+          to={`/page/${pageNum}/limit/${limit}`}
+        >
+          {limit}
+        </Link>
+      ))}
     </div>
   );
 };
