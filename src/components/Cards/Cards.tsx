dca76e1cd5874fca17f5f7d673b2edd1901c2f2b
l1senochek <lisenochek96@gmail.com>
2023-11-09 14:35:53 +0300

    feat: update Cards & styles & variables

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useEffect } from 'react';
 import Card from '../Card/Card';
 import './cards.scss';
 import {
@@ -11,20 +11,33 @@ import {
 import getAnime from '../../api/getAnime';
 import IAnime from '../../model/api/IAnime';
 import IAnimeData from '../../model/api/IAnimeData';
-import { PATH_NOT_FOUND } from '../../utils/constants/constants';
+import {
+  AVG_LIMIT_PAGES,
+  MAX_LIMIT_PAGES,
+  MIN_LIMIT_PAGES,
+  PATH_NOT_FOUND,
+} from '../../utils/constants/constants';
 import { MainPageContext } from '../../context/MainPageContext/MainPageContext';
+import { IMainPageContextState } from '../../model/context/MainPageContext/MainPageContext';
 
 const Cards: FC = (): JSX.Element => {
-  const context = useContext(MainPageContext);
+  const { arrRes, setArrRes } = useContext(
+    MainPageContext
+  ) as IMainPageContextState;
   const data = useLoaderData() as { data: Promise<IAnime> };
+
+  useEffect(() => {
+    (async () => {
+      setArrRes(await data.data);
+    })();
+  }, [data.data, setArrRes]);
 
   return (
     <Await resolve={data.data}>
       {(cards) => {
-        context?.setArrRes(cards);
         return (
           <div className="cards__wrapper">
-            {context?.arrRes?.data.length !== 0 ? (
+            {arrRes?.data.length !== 0 ? (
               cards.data.map(
                 (item: IAnimeData): JSX.Element => (
                   <Card key={item.mal_id} {...item} />
@@ -44,7 +57,10 @@ export const CardsLoader: LoaderFunction<IAnime> = async ({ params }) =>
   params.pageNum &&
   !isNaN(+params.pageNum) &&
   params.limitNum &&
-  !isNaN(+params.limitNum)
+  !isNaN(+params.limitNum) &&
+  (+params.limitNum === MIN_LIMIT_PAGES ||
+    +params.limitNum === AVG_LIMIT_PAGES ||
+    +params.limitNum === MAX_LIMIT_PAGES)
     ? defer({ data: getAnime(params.pageNum, params.limitNum, params.query) })
     : redirect(PATH_NOT_FOUND);
 
