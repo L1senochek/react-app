2917c29f7ee9d25d92c33438c19e2d956476428d
L1senochek <lisenochek96@gmail.com>
2023-11-11 14:15:40 +0300

    feat: add test getAnime with only page provided

--- src/api/getAnime.test.tsx
+++ src/api/getAnime.test.tsx
@@ -1,30 +1,41 @@
 import { afterEach, describe, expect, test, vi } from 'vitest';
 import apiResMock from '../mocks/apiResMock';
 import getAnime from './getAnime';
-import { API_URL } from '../utils/constants/constants';
+import {
+  API_LIMIT,
+  API_PAGE,
+  API_SEARCH_PARAM,
+  API_URL,
+} from '../utils/constants/constants';
 
 const fetchMock = vi.fn();
 globalThis.fetch = fetchMock;
 
 describe('getAnime: ', () => {
-  test('- correct URL parameters.', async () => {
-    fetchMock.mockResolvedValue({
-      json: vi.fn().mockResolvedValue(apiResMock),
-    });
+  const mockResolvedValue = {
+    json: vi.fn().mockResolvedValue(apiResMock),
+  };
 
+  test('- get request with correct URL parameters with all values provided.', async () => {
+    fetchMock.mockResolvedValue(mockResolvedValue);
     await getAnime(1, 25, 'naruto');
 
     expect(fetchMock).toHaveBeenCalledWith(
-      `${API_URL}?sfw=true&q=naruto&page=1&limit=25`
+      `${API_URL}?sfw=true&${API_SEARCH_PARAM}naruto&${API_PAGE}1&${API_LIMIT}25`
     );
   });
 
-  test('- getAnime equals to the mock API response.', async () => {
-    fetchMock.mockResolvedValue({
-      json: vi.fn().mockResolvedValue(apiResMock),
-    });
+  test('- get request with correct URL parameters with only page provided.', async () => {
+    fetchMock.mockResolvedValue(mockResolvedValue);
+    await getAnime(1);
 
+    expect(fetchMock).toHaveBeenCalledWith(`${API_URL}?sfw=true&${API_PAGE}1`);
+  });
+
+  test('- getAnime equals to the mock API response.', async () => {
+    fetchMock.mockResolvedValue(mockResolvedValue);
     const result = await getAnime(1, 25, 'naruto');
+
     expect(result).toEqual(apiResMock);
   });
 });
