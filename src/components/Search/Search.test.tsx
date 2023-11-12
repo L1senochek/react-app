f9050c0b7d19fb488622e94cc8e54c50fbe3377f
L1senochek <lisenochek96@gmail.com>
2023-11-12 18:28:35 +0300

    feat: add test (retrieving the value from the LS)

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -5,41 +5,52 @@ import Search from './Search';
 import { MainPageProvider } from '../../context/MainPageContext/MainPageContext';
 import { SEARCH_VALUE } from '../../utils/constants/constants';
 
-describe('Search component: ', () => {
-  test('- clicking the Search button saves the entered value to the local storage', async () => {
-    vi.mock('react-router-dom', async () => {
-      const current =
-        await vi.importActual<typeof import('react-router-dom')>(
-          'react-router-dom'
-        );
-      const mockNavigate = vi.fn();
-      return {
-        ...current,
-        useParams: (): Readonly<Params<string>> => ({
-          limit: '25',
-        }),
-        useNavigate: (): typeof mockNavigate => mockNavigate,
-      };
-    });
+describe('Search: ', () => {
+  vi.mock('react-router-dom', async () => {
+    const current =
+      await vi.importActual<typeof import('react-router-dom')>(
+        'react-router-dom'
+      );
+    const mockNavigate = vi.fn();
+    return {
+      ...current,
+      useParams: (): Readonly<Params<string>> => ({
+        limit: '25',
+      }),
+      useNavigate: (): typeof mockNavigate => mockNavigate,
+    };
+  });
 
-    const routes = [
-      {
-        path: '/mock/path',
-        element: <Search />,
-      },
-    ];
+  const routes = [
+    {
+      path: '/mock/path',
+      element: <Search />,
+    },
+  ];
 
-    const router = createMemoryRouter(routes, {
-      initialEntries: ['/mock/path'],
-    });
+  const router = createMemoryRouter(routes, {
+    initialEntries: ['/mock/path'],
+  });
 
+  localStorage.setItem(SEARCH_VALUE, 'testValue2');
+
+  test('- check that the component retrieves the value from the local storage upon mounting.', async () => {
     render(
       <MainPageProvider>
         <RouterProvider router={router} />
       </MainPageProvider>
     );
+    const inputElement = screen.getByDisplayValue('testValue2');
+    expect(inputElement).toBeDefined();
+  });
 
-    const inputElement = screen.getByPlaceholderText('Search...');
+  test('- clicking the Search button saves the entered value to the local storage.', async () => {
+    render(
+      <MainPageProvider>
+        <RouterProvider router={router} />
+      </MainPageProvider>
+    );
+    const inputElement = screen.getByDisplayValue('testValue2');
     fireEvent.change(inputElement, { target: { value: 'testValue' } });
     fireEvent.keyUp(inputElement, { key: 'Enter' });
     const storedValue = localStorage.getItem(SEARCH_VALUE);
