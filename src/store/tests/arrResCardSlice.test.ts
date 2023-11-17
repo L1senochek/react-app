c9a149cd2ff745e2bac0d635c4f3da39cf5802d1
l1senochek <lisenochek96@gmail.com>
2023-11-17 16:35:49 +0300

    feat: add arrResCardSlice.test.ts

---
+++ src/store/tests/arrResCardSlice.test.ts
@@ -0,0 +1,15 @@
+import { assert, describe, test } from 'vitest';
+import arrResCardReducer, { setArrResCard } from '../arrResCardSlice';
+import apiResDataMock from '../../mocks/apiResDataMock';
+
+describe('arrResCardSlice: ', () => {
+  test('- arrResCard is updated correctly', () => {
+    const initialState = { arrResCard: undefined };
+    const newState = arrResCardReducer(
+      initialState,
+      setArrResCard(apiResDataMock)
+    );
+
+    assert.equal(newState.arrResCard, apiResDataMock);
+  });
+});
