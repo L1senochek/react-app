130d21f082dfbe67e4be51cd1efb6230dcbd62f3
l1senochek <lisenochek96@gmail.com>
2023-11-23 12:11:25 +0300

    feat: update description & base styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../styles/globals.scss';
+import style from '../styles/base/base.module.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Anime catalog',
   description:
-    'This is an anime catalog app. Here you can find any anime to your taste, with detailed descriptions and ratings',
+    'This is an anime catalog app. Here you can find any anime to your taste, with detailed descriptions and ratings.',
   keywords:
     'anime, catalog-anime, anime-catalog, anime-detailed-descriptions, anime-ratings',
 };
@@ -19,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${style['body']}`}>{children}</body>
     </html>
   );
 }
